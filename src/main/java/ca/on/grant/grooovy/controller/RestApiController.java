2d722dc318e5620ce2bfeebebe67ec86f2a374a3
Grant <108592892+GrantProductions@users.noreply.github.com>
2022-07-15 16:06:40 -0400

    RestApiController: organize imports.

--- src/main/java/ca/on/grant/grooovy/controller/RestApiController.java
+++ src/main/java/ca/on/grant/grooovy/controller/RestApiController.java
@@ -1,11 +1,8 @@
 package ca.on.grant.grooovy.controller;
 
-import java.security.Principal;
 import java.util.Locale;
 import java.util.Map;
 import java.util.stream.Collectors;
-
-import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,8 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
