75575c95961ac5d344c85dfd1e7167aeb8154dc7
Grant <108592892+GrantProductions@users.noreply.github.com>
2022-07-15 13:39:43 -0400

    RecessHacks2022Application + RecessHacks2022ApplicationTests.

---
+++ src/main/java/ca/on/grant/recesshacks/y2022/RecessHacks2022Application.java
@@ -0,0 +1,13 @@
+package ca.on.grant.recesshacks.y2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecessHacks2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecessHacks2022Application.class, args);
+	}
+
+}
---
+++ src/test/java/ca/on/grant/recesshacks/y2022/RecessHacks2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.on.grant.recesshacks.y2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecessHacks2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
