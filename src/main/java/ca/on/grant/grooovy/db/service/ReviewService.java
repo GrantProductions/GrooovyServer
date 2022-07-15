d75cd298648d1a6d80112b8e901098f8a35c3587
Grant <108592892+GrantProductions@users.noreply.github.com>
2022-07-15 19:39:04 -0400

    ReviewService

---
+++ src/main/java/ca/on/grant/grooovy/db/service/ReviewService.java
@@ -0,0 +1,12 @@
+package ca.on.grant.grooovy.db.service;
+
+import java.util.List;
+
+import ca.on.grant.grooovy.db.entity.Review;
+import ca.on.grant.grooovy.db.entity.User;
+import ca.on.grant.grooovy.response.GenericResponse;
+
+public interface ReviewService {
+	List<Review> getReviewsByUrl(String url);
+	GenericResponse addReview(User user, String url, String numOfStars, String text);
+}
---
+++ src/main/java/ca/on/grant/grooovy/db/service/ReviewServiceImpl.java
@@ -0,0 +1,66 @@
+package ca.on.grant.grooovy.db.service;
+
+import java.util.List;
+
+import org.apache.commons.validator.routines.UrlValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ca.on.grant.grooovy.db.entity.Review;
+import ca.on.grant.grooovy.db.entity.User;
+import ca.on.grant.grooovy.db.repository.ReviewRepository;
+import ca.on.grant.grooovy.response.GenericResponse;
+
+@Service
+public class ReviewServiceImpl implements ReviewService{
+	@Autowired
+	private ReviewRepository reviewRepository;
+	
+	public List<Review> getReviewsByUrl(String url){
+		return reviewRepository.findByUrl(url);
+	}
+
+	@Override
+	public GenericResponse addReview(User user, String url, String numOfStars, String text) {
+		if(numOfStars == null) {
+			return new GenericResponse(false, "Please specify a rating");
+		}
+		numOfStars = numOfStars.trim();
+		if(numOfStars.length() == 0) {
+			return new GenericResponse(false, "Please specify a rating");
+		}else if(!isValidNum(numOfStars)) {
+			return new GenericResponse(false, "Invalid rating");
+		}else {
+			int parsedNum = Integer.parseInt(numOfStars);
+			if(parsedNum < 1 || parsedNum > 5) {
+				return new GenericResponse(false, "Invalid rating");
+			}
+		}
+		if(url == null) {
+			return new GenericResponse(false, "Please specify a URL");
+		}
+		url = url.trim();
+		if(url.length() == 0) {
+			return new GenericResponse(false, "Please specify a URL");
+		}else if(!new UrlValidator().isValid(url)) {
+			return new GenericResponse(false, "Invalid URL");
+		}
+		
+		if(text == null) {
+			text = "";
+		}else {
+			text = text.trim();
+		}
+		
+		return null;
+	}
+	
+	private boolean isValidNum(String num) {
+		try {
+			Integer.parseInt(num);
+			return true;
+		}catch(Exception e) {
+			return false;
+		}
+	}
+}
